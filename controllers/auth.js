91c8b13830e6ff0a98ca0ec783cbcf3f1df37747
shosetim <shosetim32@gmail.com>
2022-07-21 18:05:39 +0800

    updated job

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,16 +1,30 @@
 const { StatusCodes } = require('http-status-codes');
 const User = require('../models/User');
-const { BadRequestError } = require('../errors');
-const bcrypt = require('bcryptjs');
+const { BadRequestError, UnauthenticatedError } = require('../errors');
 
 const register = async (req, res) => {
   const user = new User({ ...req.body });
+  const token = user.createJWT();
   await user.save();
-  res.status(StatusCodes.CREATED).json({ msg: 'user created', user });
+  console.log(token)
+  res.status(StatusCodes.CREATED).json({ user: { name: user.name }, token });
 }
 
 const login = async (req, res) => {
-  res.send('login user')
+  const { email, password } = req.body;
+  if (!email || !password) {
+    throw new BadRequestError('Please provide email and password');
+  }
+  const user = await User.findOne({ email });
+  if (!user) {
+    throw new UnauthenticatedError('Invalid Credentials');
+  }
+  const isPasswordCorrect = await user.comparePassword(password);
+  if (!isPasswordCorrect) {
+    throw new UnauthenticatedError('Invalid Credentials');
+  }
+  const token = user.createJWT();
+  res.status(StatusCodes.OK).json({ user: { name: user.name }, token })
 }
 
 module.exports = {
--- controllers/jobs.js
+++ controllers/jobs.js
@@ -1,17 +1,52 @@
+const Job = require('../models/Job');
+const { StatusCodes } = require('http-status-codes');
+const { BadRequestError, NotFoundError } = require('../errors');
+
 const getAllJobs = async (req, res) => {
-  res.send('getAllJobs')
+  const jobs = await Job.find({ createdBy: req.user.userId}).sort('createdAt');
+  res.status(StatusCodes.OK).json({ jobs, count: jobs.length });
 }
 
 const getJob = async (req, res) => {
-  res.send('get Job')
+  const { user: { userId }, params: { id: jobId } } = req;
+  const job = await Job.findOne({ _id: jobId, createdBy: userId });
+  if (!job) {
+    throw new NotFoundError(`No job with id ${jobId}`)
+  }
+  res.status(StatusCodes.OK).json({ job })
 }
 
 const createJob = async (req, res) => {
-  res.send('create Job')
+  req.body.createdBy = req.user.userId;
+  const job = new Job({ ...req.body });
+  await job.save();
+  res.status(StatusCodes.CREATED).json({
+    msg: 'job created',
+    job
+  })
 }
 
 const updateJob = async (req, res) => {
-  res.send('update Job')
+  const { 
+    user: { userId }, 
+    params: { id: jobId },
+    body: { company, position }
+  } = req;
+  if (company === '' || position === '') {
+    throw new BadRequestError('Company or position field cannot be empty')
+  }
+  const job = await Job.findOne({ _id: jobId });
+  console.log(job)
+  if (!job) {
+    throw new NotFoundError(`No job with id ${jobId}`);
+  }
+  job.company = company;
+  job.position = position;
+  await job.save();
+  res.status(StatusCodes.OK).json({
+    msg: 'success updated job',
+    job,
+  })
 }
 
 const deleteJob = async (req, res) => {
